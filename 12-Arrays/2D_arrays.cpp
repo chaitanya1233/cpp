21413ecb2dd9ddfd3a62922dfe0e2425f65dd70a
Chaitanya <ckale5156@gmail.com>
2026-05-29 14:14:15 +0530

    modified 2_d array

--- 12-Arrays/2D_arrays.cpp
+++ 12-Arrays/2D_arrays.cpp
@@ -6,17 +6,18 @@ int main()
 {
 
     // Declare two dimenstional array
+    const int rows =  3 , cols = 3;
 
-    int two_dim_array[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int two_dim_array[rows][cols] = {{1,2,3},{4,5,6},{7,8,9}};
 
     // iterate over elements to print it
 
     // for loop
     int n = sizeof(two_dim_array)/sizeof(two_dim_array[0]);
     cout<<"Two dimensional array is:"<<endl;
-    for(int i=0;i<n;i++)
+    for(int i=0;i<rows;i++)
     {
-        for(int j=0;j<n;j++)
+        for(int j=0;j<cols;j++)
         {
             cout<<two_dim_array[i][j]<<" ";
         }
